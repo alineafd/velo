dff45a56e407afe3da42b21c50fd7521deadafed
alineafd <alineafd.dias@gmail.com>
2026-03-04 21:25:06 -0300

    comitando desafio sobreviva sem Data Test IDs

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,9 +4,9 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('VLO-LRG3MN');
-  await page.getByTestId('search-order-button').click();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-LRG3MN');
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-LRG3MN');
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  await page.getByText('VLO-LRG3MN');
+  await page.getByText('APROVADO');
 });
